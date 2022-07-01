fa00eb4a8affb02b0a7b5f6c63a572e244447cbc
nnvedward <nnvedward@gmail.com>
2022-07-01 15:31:11 +0100

    Doubly linked list: Get method

--- Data-Structures/doublyLinkedList.js
+++ Data-Structures/doublyLinkedList.js
@@ -67,10 +67,30 @@ class DoublyLinkedList {
         this.length++
         return this
     }
+    get(index) {
+        if (index < 0 || index >= this.length) return null
+        let count, current
+        if (index <= this.length / 2) {
+            count = 0
+            current = this.head
+            while (count !== index) {
+                current = current.next
+                count++
+            }
+        } else {
+            count = this.length - 1
+            current = this.tail
+            while (count !== index) {
+                current = current.prev
+                count--
+            }
+        }
+        return current
+    }
 }
 
 const list = new DoublyLinkedList()
 list.push('Hi')
 list.push('Hello')
 list.push('Goodbye')
-list.unShift('Hey')
+list.get(1)
