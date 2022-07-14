a4204bdeddf48cf837d9fccaa501b83da6c17d27
nnvedward <nnvedward@gmail.com>
2022-07-14 11:58:49 +0100

    Hash tables: Improve hash function

--- Data-Structures/hashTables/hash.js
+++ Data-Structures/hashTables/hash.js
@@ -1,12 +1,14 @@
 // Hash function
 function hash(key, arraylen) {
     let total = 0
-    for (let char of key) {
+    let PRIME = 31
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i]
         // map "a" to 1, "b" to 2, "c" to 3, etc
         let value = char.charCodeAt(0) - 96
-        total = (total + value) % arraylen
+        total = (total * PRIME + value) % arraylen
     }
     return total
 }
 
-console.log(hash("prime", 10))
+console.log(hash("prime", 13))
