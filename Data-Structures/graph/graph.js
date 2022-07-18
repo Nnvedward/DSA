870909ea046c5c85eecf2d9ea518c5a8da2af27e
nnvedward <nnvedward@gmail.com>
2022-07-18 15:54:10 +0100

    Graphs: Add edge

--- Data-Structures/graph/graph.js
+++ Data-Structures/graph/graph.js
@@ -7,7 +7,16 @@ class Graph {
     addVertex(vertex) {
        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
     }
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2)
+        this.adjacencyList[vertex2].push(vertex1)
+        return this
+    }
 }
 
 let graph = new Graph()
 graph.addVertex('Tokyo')
+graph.addVertex('Nairobi')
+graph.addVertex('Oslo')
+graph.addEdge('Tokyo', 'Nairobi')
+graph.addEdge('Nairobi', 'Oslo')
