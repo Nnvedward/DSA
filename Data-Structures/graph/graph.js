1d2a16053035de99b539ec06c18f01ade80a0915
nnvedward <nnvedward@gmail.com>
2022-07-19 16:10:20 +0100

    Graphs: Remove vertex

--- Data-Structures/graph/graph.js
+++ Data-Structures/graph/graph.js
@@ -20,12 +20,24 @@ class Graph {
             v => v !== vertex1
         )
     }
+    removeVertex(vertex) {
+        while (this.adjacencyList[vertex].length) {
+            const adjacenctVertex = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex, adjacenctVertex)
+        }
+        delete this.adjacencyList[vertex]
+    }
 }
 
 let graph = new Graph()
 graph.addVertex('Tokyo')
 graph.addVertex('Nairobi')
 graph.addVertex('Oslo')
+graph.addVertex('Denver')
+graph.addVertex('Rio')
 graph.addEdge('Nairobi', 'Tokyo')
 graph.addEdge('Nairobi', 'Oslo')
-graph.removeEdge('Nairobi', 'Oslo')
+graph.addEdge('Denver', 'Rio')
+graph.addEdge('Oslo', 'Denver')
+graph.addEdge('Rio', 'Nairobi')
+graph.removeVertex('Nairobi')
