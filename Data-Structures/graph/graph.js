dbf29a7350f9e51d388d91dd051a9ac35cb57dc5
nnvedward <nnvedward@gmail.com>
2022-07-19 15:47:33 +0100

    Graphs: Remove edge

--- Data-Structures/graph/graph.js
+++ Data-Structures/graph/graph.js
@@ -12,11 +12,20 @@ class Graph {
         this.adjacencyList[vertex2].push(vertex1)
         return this
     }
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        )
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        )
+    }
 }
 
 let graph = new Graph()
 graph.addVertex('Tokyo')
 graph.addVertex('Nairobi')
 graph.addVertex('Oslo')
-graph.addEdge('Tokyo', 'Nairobi')
+graph.addEdge('Nairobi', 'Tokyo')
 graph.addEdge('Nairobi', 'Oslo')
+graph.removeEdge('Nairobi', 'Oslo')
